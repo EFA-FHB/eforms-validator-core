3aa2e7559f0e89f383aa07892b792c54d11c38f1
Darius Matulis <darius.matulis@nortal.com>
2023-11-08 09:41:26 +0200

    hotfix/D603345-5203 for compliance eForms SDK 1.7.0 version was changed to 1.7.2

--- src/main/java/com/nortal/efafhb/eforms/validator/enums/SupportedVersion.java
+++ src/main/java/com/nortal/efafhb/eforms/validator/enums/SupportedVersion.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 @Getter
 public enum SupportedVersion {
   V1_5_5("1.5.5"),
-  V1_7_0("1.7.0"),
+  V1_7_2("1.7.2"),
   V1_0_1("1.0.1"),
   V1_0_0("1.0.0"),
   V0_1_1("0.1.1"),
@@ -18,7 +18,7 @@ public enum SupportedVersion {
   private static final Map<SupportedVersion, SupportedVersion> sdkVersionsForGdk =
       Map.of(
           SupportedVersion.V1_0_1, SupportedVersion.V1_5_5,
-          SupportedVersion.V1_1_0, SupportedVersion.V1_7_0);
+          SupportedVersion.V1_1_0, SupportedVersion.V1_7_2);
 
   SupportedVersion(String value) {
     this.value = value;
