9b2ea52bcbfd933ff5055fed63164697fb474b02
Aleksandra Milenkovic <aleksandra.milenkovic@nortal.com>
2023-12-12 17:24:03 +0100

    D603345-5292 adding test

--- src/test/java/com/nortal/efafhb/eforms/validator/validation/service/ValidatorServiceImplTest.java
+++ src/test/java/com/nortal/efafhb/eforms/validator/validation/service/ValidatorServiceImplTest.java
@@ -8,6 +8,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
+import com.nortal.efafhb.eforms.validator.enums.SupportedType;
+import com.nortal.efafhb.eforms.validator.enums.SupportedVersion;
 import com.nortal.efafhb.eforms.validator.validation.FormsValidator;
 import com.nortal.efafhb.eforms.validator.validation.ValidationConfig;
 import com.nortal.efafhb.eforms.validator.validation.dto.BusinessDocumentValidationModelDTO;
@@ -25,6 +27,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 @QuarkusTest
@@ -38,19 +41,17 @@ class ValidatorServiceImplTest {
 
   @Inject ValidationConfig validationConfig;
   @Mock FormsValidator formsValidator;
-  @Mock EFormsParser eFormsParser;
 
   @BeforeEach
   void setUp() {
-    MockitoAnnotations.initMocks(this);
+    MockitoAnnotations.openMocks(this);
     validatorService =
         new ValidatorServiceImpl(businessDocumentValidator, validationConfig, formsValidator);
   }
 
   @Test
-  void testValidateWithSchematronValidation() {
+  void testValidateWithValidationsDisabled() {
     ValidationRequestDTO validationRequestDTO = new ValidationRequestDTO();
-    validationRequestDTO.setSchematronValidation(true);
     validationRequestDTO.setEforms(new byte[] {});
     validationRequestDTO.setVersion("1.0");
     validationRequestDTO.setSdkType("eforms-de");
@@ -59,6 +60,30 @@ class ValidatorServiceImplTest {
 
     ValidationModelDTO result = validatorService.validate(validationRequestDTO);
     assertNull(result);
+  }
+
+  @Test
+  void testValidateWithSchematronValidation() throws IOException {
+    ValidationRequestDTO validationRequestDTO = new ValidationRequestDTO();
+    validationRequestDTO.setEforms(readEFormAsByteArray(EFORM_WITH_ERRORS_PATH));
+    validationRequestDTO.setVersion("1.0");
+    validationRequestDTO.setSdkType("eforms-de");
+    validationRequestDTO.setXsdValidation(false);
+    validationRequestDTO.setSchematronValidation(true);
+
+    ValidationResult result = new ValidationResult();
+    result.setSdkValidationVersion("eforms-de-1.0");
+    Mockito.when(
+            formsValidator.validate(
+                Mockito.any(SupportedType.class),
+                Mockito.anyString(),
+                Mockito.any(SupportedVersion.class)))
+        .thenReturn(result);
+    ValidationModelDTO validationModel = validatorService.validate(validationRequestDTO);
+    assertNotNull(validationModel);
+    assertTrue(validationModel.getValid());
+    assertTrue(validationModel.getWarnings().isEmpty());
+    assertTrue(validationModel.getErrors().isEmpty());
   }
 
   @Test
@@ -90,7 +115,6 @@ class ValidatorServiceImplTest {
             + "is expected.'";
 
     ValidationRequestDTO validationRequestDTO = new ValidationRequestDTO();
-    validationRequestDTO.setSchematronValidation(true);
     validationRequestDTO.setEforms(readEFormAsByteArray(EFORM_WITH_ERRORS_PATH));
     validationRequestDTO.setVersion("1.0");
     validationRequestDTO.setSdkType("eforms-de");
