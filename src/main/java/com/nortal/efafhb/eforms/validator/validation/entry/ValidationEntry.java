57e4ed871153cc08609b12be5a9fb54e3aa5eec3
Gediminas <118281897+gediminasre@users.noreply.github.com>
2024-02-06 18:05:02 +0200

    D603345-01: D605756-2: eforms-de-1.2 implementation and sdk 1.10.1 update

--- src/main/java/com/nortal/efafhb/eforms/validator/validation/entry/ValidationEntry.java
+++ src/main/java/com/nortal/efafhb/eforms/validator/validation/entry/ValidationEntry.java
@@ -2,12 +2,10 @@ package com.nortal.efafhb.eforms.validator.validation.entry;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.ToString;
 import lombok.With;
 
 @Builder
 @Getter
-@ToString
 public class ValidationEntry {
   private String rule;
   private String path;
